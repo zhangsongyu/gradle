0b4f5133a41d0cb32ca2a027d71d143c97bd5e3b
张凇毓 <zhangsongyu@tpulse-tech.com>
2019-08-06 16:34:48 +0800

    dsfd

--- src/test/java/music/Lyrics.java
+++ src/test/java/music/Lyrics.java
@@ -8,6 +8,8 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zsy on 2019/8/6 14:59
@@ -26,12 +28,17 @@ public class Lyrics {
                 .build();
 
         Response songListResp = client.newCall(songsList).execute();
+        String songListStr = songListResp.body().string();
+        Map<String, Object> songMap = JSON.parseObject(songListStr);
+        Map resultMap = JSON.parseObject(songMap.get("result").toString());
+        String listname = resultMap.get("name").toString();
 
-        SongVO.SongListResp songList = JSON.parseObject(songListResp.body().string(), SongVO.SongListResp.class);
+        List<Map> tracks = JSON.parseObject(resultMap.get("tracks").toString(), List.class);
 
-        songList.getResult().getTracks().forEach(track -> {
+
+        tracks.forEach(track -> {
             Request lyric = new Request.Builder()
-                    .url("http://music.163.com/api/song/lyric?id=" + track.getId() + "&lv=1&kv=1&tv=-1")
+                    .url("http://music.163.com/api/song/lyric?id=" + track.get("id") + "&lv=1&kv=1&tv=-1")
                     .get()
                     .addHeader("cache-control", "no-cache")
                     .addHeader("postman-token", "b3a432e9-ff84-73cc-a34c-d40fc30c4c86")
@@ -39,9 +46,11 @@ public class Lyrics {
 
             try {
                 Response lyricResp = client.newCall(lyric).execute();
-                SongVO.LrcResp lrc = JSON.parseObject(lyricResp.body().string(), SongVO.LrcResp.class);
-                FileUtils.writeStringToFile(new File(FILE_PATH + songList.getResult().getName()),
-                        lrc.getLrc().getLyric().getLyric(), "utf-8");
+                String lrcStr = lyricResp.body().string();
+                Map lrcMap = JSON.parseObject(lrcStr);
+                String lyricStr = JSON.parseObject(lrcMap.get("lrc").toString()).get("lyric").toString();
+                FileUtils.writeStringToFile(new File(FILE_PATH + listname+"\\"+track.get("name").toString()),
+                        lyricStr, "utf-8");
             } catch (IOException e) {
                 e.printStackTrace();
             }
